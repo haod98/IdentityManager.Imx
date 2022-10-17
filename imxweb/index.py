0dab6d8798fd5aff50f68eb20b2e0f370041e5fe
Hao Dong <hao.dong@itdesign.at>
2022-10-17 10:44:41 +0200

    Add script to select modules to build

---
+++ imxweb/index.py
@@ -0,0 +1,26 @@
+import inquirer
+import os
+
+CHECKBOX_NAME = 'modules'
+questions = [
+    inquirer.Checkbox(CHECKBOX_NAME,
+                      message="which modules do you need? (Please consider the order)",
+                      choices=['qbm', 'qer', 'tsb', 'att',
+                               'rms', 'rps', 'aad', 'aob',
+                               'uci', 'cpl', 'hds', 'dpr',
+                               'o3t', 'olg', 'pol'],
+                      carousel=True
+                      )
+]
+selectedModules = inquirer.prompt(questions)[CHECKBOX_NAME]
+lastModuleSelected = selectedModules[len(selectedModules)-1]
+commandToExecute = ''
+
+for module in selectedModules:
+    if lastModuleSelected == module:
+        commandToExecute += 'npm run build ' + module + ' && npm start'
+    else:
+        commandToExecute += 'npm run build ' + module + ' && '
+
+print(commandToExecute)
+# os.system(commandToExecute)
